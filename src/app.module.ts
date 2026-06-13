582218f89f2233a72f342bcc8436ef1f16fa3f83
AnisurRahman06046 <anisurrahman06046@gmail.com>
2026-06-13 16:25:15 +0600

    folder structured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Module } from './tenant/.module';
+import { TenantModule } from './modules/tenant/tenant.module';
+import { FileModule } from './modules/file/file.module';
+import { StorageModule } from './modules/storage/storage.module';
+import { UploadModule } from './modules/upload/upload.module';
 
 @Module({
-  imports: [],
+  imports: [Module, TenantModule, FileModule, StorageModule, UploadModule],
   controllers: [AppController],
   providers: [AppService],
 })
